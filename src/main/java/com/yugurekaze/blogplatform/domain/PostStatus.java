da8600d7bc3b35b17c43dd00e15ec6d4a3d3abcb
YugureKaze <yotsume52@gmail.com>
2026-01-02 18:36:54 +0300

    Add Post entity and PostStatus enum

---
+++ src/main/java/com/yugurekaze/blogplatform/domain/PostStatus.java
@@ -0,0 +1,6 @@
+package com.yugurekaze.blogplatform.domain;
+
+public enum PostStatus {
+    DRAFT,
+    PUBLISHED,
+}
---
+++ src/main/java/com/yugurekaze/blogplatform/domain/entities/Post.java
@@ -0,0 +1,95 @@
+package com.yugurekaze.blogplatform.domain.entities;
+
+import com.yugurekaze.blogplatform.domain.PostStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.proxy.HibernateProxy;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class Post {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+
+    @Column(nullable = false)
+    private Integer readingTime;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Version
+    private Long version;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "post_tags",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+
+    @PreUpdate
+    protected void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+    @PrePersist
+    protected void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy
+                ? proxy.getHibernateLazyInitializer().getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy
+                ? proxy.getHibernateLazyInitializer().getPersistentClass()
+                : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Post post = (Post) o;
+        return getId() != null && Objects.equals(getId(), post.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy
+                ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
+
+}
