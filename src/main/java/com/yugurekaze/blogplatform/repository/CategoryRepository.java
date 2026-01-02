e73e6ce35e8adfa1e441dc0c78e09b3ac99c2842
YugureKaze <yotsume52@gmail.com>
2026-01-02 21:33:00 +0300

    Implement fetch all categories with posts

--- src/main/java/com/yugurekaze/blogplatform/repository/CategoryRepository.java
+++ src/main/java/com/yugurekaze/blogplatform/repository/CategoryRepository.java
@@ -2,9 +2,15 @@ package com.yugurekaze.blogplatform.repository;
 
 import com.yugurekaze.blogplatform.domain.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+    @Query("SELECT c FROM Category c JOIN FETCH c.posts")
+    List<Category> findAllWithPostCount();
 }
